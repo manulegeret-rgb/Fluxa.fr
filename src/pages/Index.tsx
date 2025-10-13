f77927dc2828f1087f4f17faec165c5916494022
manulegeret-rgb <manu.legeret@gmail.com>
2025-10-13 17:28:55 +0200

    Fix: agrandissement du logo mobile Fluxa

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -130,7 +130,7 @@ Merci !`
     <img
       src={fluxaLogo}
       alt="Fluxa"
-      className="h-40 w-auto object-contain -translate-y-[14px]"
+      className="h-40 w-auto object-contain -translate-y-[16px]"
     />
   </a>
 </div>
