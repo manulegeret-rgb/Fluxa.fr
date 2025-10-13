82f08076f50008ed85ff0cd206104c685a3beb30
manulegeret-rgb <manu.legeret@gmail.com>
2025-10-13 20:16:46 +0200

    Fix: agrandissement du logo mobile Fluxa

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -457,7 +457,7 @@ Merci !`
           <form
   id="contact"
   onSubmit={onSubmitInfo}
-  className="mt-10 max-w-2xl mx-auto space-y-4 scroll-mt-[60px] md:scroll-mt-24"
+  className="mt-10 max-w-2xl mx-auto space-y-4 scroll-mt-[100px] md:scroll-mt-24"
 >
             <div className="grid sm:grid-cols-2 gap-4">
               <div>
